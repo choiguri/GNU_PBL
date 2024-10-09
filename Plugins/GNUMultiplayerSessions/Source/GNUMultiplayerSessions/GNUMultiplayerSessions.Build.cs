6ee7b011f47c72db33ada580dc57a1df3b241b7f
배고픈사내 <pju0205@naver.com>
2024-10-10 04:46:53 +0900

    24.10.10. 메뉴 위젯 생성
    메뉴 위젯을 위한 C++ 클래스 생성
    위젯 블루프린트 생성 및 버튼 추가

--- Plugins/GNUMultiplayerSessions/Source/GNUMultiplayerSessions/GNUMultiplayerSessions.Build.cs
+++ Plugins/GNUMultiplayerSessions/Source/GNUMultiplayerSessions/GNUMultiplayerSessions.Build.cs
@@ -28,6 +28,9 @@ public class GNUMultiplayerSessions : ModuleRules
 				"Core",
 				"OnlineSubsystem",
 				"OnlineSubsystemSteam",
+				"UMG",
+				"Slate",
+				"SlateCore",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
